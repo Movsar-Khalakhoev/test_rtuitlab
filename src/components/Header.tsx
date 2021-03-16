5a976e327aed47783db1ba90a69cd6bea2179cac
Movsar Khalakhoev <movsar.kh.128@gmail.com>
2021-03-16 06:39:44 +0300

    Поправлена шапка сайта

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import { Button, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='#home'>Новости.ру</Navbar.Brand>
+      <Navbar.Brand>
+        <Link to='/'>Новости.ру</Link>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Главная</Nav.Link>
-          <Nav.Link href='#link'>Link</Nav.Link>
-          <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-            <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link>
+            <Link to='/'>Главная</Link>
+          </Nav.Link>
         </Nav>
         <Form inline>
-          <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-          <Button variant='outline-success'>Search</Button>
+          <FormControl type='text' placeholder='Введите название поста' className='mr-sm-2' />
+          <Button variant='outline-success'>Найти</Button>
         </Form>
       </Navbar.Collapse>
     </Navbar>
